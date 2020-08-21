d28a2b408f64db4e229cc1f7183771e26d8345e2
panda-satyabrata <satyabrat.panda97@gmail.com>
2020-08-22 03:10:18 +0530

    Updates

---
+++ precache-manifest.96feb93e52cdc6a10dace39fe8975471.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7153ae7b1379bdd36117bcd7ba1e63d2",
+    "url": "/design-prac/index.html"
+  },
+  {
+    "revision": "1e907f126287fd9f4a63",
+    "url": "/design-prac/static/css/main.23eca62d.chunk.css"
+  },
+  {
+    "revision": "d5f1751a9eda056f6ba3",
+    "url": "/design-prac/static/js/2.4c0e7ed8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/design-prac/static/js/2.4c0e7ed8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e907f126287fd9f4a63",
+    "url": "/design-prac/static/js/main.861975ab.chunk.js"
+  },
+  {
+    "revision": "0c00576dda9554514edf",
+    "url": "/design-prac/static/js/runtime-main.5096fba3.js"
+  }
+]);
